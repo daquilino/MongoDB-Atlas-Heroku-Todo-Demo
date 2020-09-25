ee86f7af0bdc995511129fd1323fc1e94a4926b9
D <douglasjaquilino@gmail.com>
2020-09-25 08:25:42 -0400

    changed route from all to get, added comment

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,6 +1,7 @@
 const Router = require('express').Router();
 const Path = require('path')
 
-Router.all("/*", (req, res) => res.sendFile(Path.join(__dirname, '../index.html')))
+//Any get requests not matched by other routes will server the index.html page
+Router.get("/*", (req, res) => res.sendFile(Path.join(__dirname, '../index.html')))
 
 module.exports = Router;
